35abf6b3870ccf8d05b4cb36e5baf9c759dfb1ed
Jean Guillotin <jguillotin2@gmail.com>
2024-04-14 00:43:47 -0400

    Record Data using SPIFFS

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
-// Basic OLED demo for accelerometer readings from Adafruit MPU6050
-
 // ESP32 Guide: https://RandomNerdTutorials.com/esp32-mpu-6050-accelerometer-gyroscope-arduino/
-// ESP8266 Guide: https://RandomNerdTutorials.com/esp8266-nodemcu-mpu-6050-accelerometer-gyroscope-arduino/
-// Arduino Guide: https://RandomNerdTutorials.com/arduino-mpu-6050-accelerometer-gyroscope/
 
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_Sensor.h>
+#include <ArduinoJson.h>
+#include <SPIFFS.h>
 
 #define MAGNET_SW_PIN 19
+#define button 13
 
 Adafruit_MPU6050 mpu;
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
@@ -17,10 +16,20 @@ sensors_event_t a, g, temp;
 
 int rpm = 0;
 
+JsonDocument docWrite, docRead;
+const char* filename = "/data.json";
+
+int recordCount = 0; // goes however long until reading stops
+bool buttonONS = false;
+bool recordData = false;
+
 /* Declared Functions */
 static void calcRPM(void*);
 double getPitch();
 double getRoll();
+void readDataFromFile();
+void printFileData();
+void writeDataToFile();
 /* End of Declared functions*/
 
 void setup() {
@@ -55,41 +64,82 @@ void setup() {
                     0,       /* Priority of the task */
                     NULL,    /* Task handle. */
                     0);  /* Core where the task should run */
+
+  // Initialize the SPIFFS object
+  if(!SPIFFS.begin(true)) {
+    Serial.println("Error initializing SPIFFS");
+    while (1)
+      yield();
+  }
+  readDataFromFile();
 }
 
 void loop() {
+  /* Read values from mpu6050 Sensor */
   mpu.getEvent(&a, &g, &temp);
 
+  double accX = a.acceleration.x;
+  double accY = a.acceleration.y;
+  double accZ = a.acceleration.z;
+  double gyroX = g.gyro.x;
+  double gyroY = g.gyro.y;
+  double gyroZ = g.gyro.z;
+
+  /* Displaying Data on OLED */
   display.clearDisplay();
   display.setCursor(0, 0);
 
   display.println("Accelerometer - m/s^2");
-  display.print(a.acceleration.x, 1);
+  display.print(accX, 1);
   display.print(", ");
-  display.print(a.acceleration.y, 1);
+  display.print(accY, 1);
   display.print(", ");
-  display.print(a.acceleration.z, 1);
+  display.print(accZ, 1);
   display.println("");
 
   display.println("Gyroscope - rps");
-  display.print(g.gyro.x, 1);
+  display.print(gyroX, 1);
   display.print(", ");
-  display.print(g.gyro.y, 1);
+  display.print(gyroY, 1);
   display.print(", ");
-  display.print(g.gyro.z, 1);
+  display.print(gyroZ, 1);
   display.println("");
 
-  display.println("Orientation - degrees");
-  display.print("Pitch: ");
+  display.print("Pitch (deg): ");
   display.println(getPitch());
-  display.print("Roll: ");
-  display.print(getRoll());
-  display.println("");
+  display.print("Roll (deg): ");
+  display.println(getRoll());
 
   display.print("RPMs: ");
-  display.print(rpm);
+  display.println(rpm);
 
   display.display();
+
+  /* Recording Data using SPIFFS */
+  if(!buttonONS && touchRead(button) < 50){
+    Serial.println("Button Press!");
+    buttonONS = true;
+    recordData = !recordData;
+    if(recordData == false){
+      recordCount = 0;
+      writeDataToFile();
+      readDataFromFile();
+    }
+  }
+  if(touchRead(button) > 50){
+    buttonONS = false;
+  }
+
+  if(recordData){
+    docWrite["acc"]["x"][recordCount] = accX;
+    docWrite["acc"]["y"][recordCount] = accY;
+    docWrite["acc"]["z"][recordCount] = accZ;
+    docWrite["gyro"]["x"][recordCount] = gyroX;
+    docWrite["gyro"]["y"][recordCount] = gyroY;
+    docWrite["gyro"]["z"][recordCount] = gyroZ;
+    recordCount++;
+  }
+
   delay(100);
 }
 
@@ -155,4 +205,49 @@ double getRoll(){
   }else{
     return NAN;
   }
+}
+
+// Updates and prints docRead with acceleration and rotation data during the recording time period
+void readDataFromFile() {
+  // Read JSON data from a file
+  File file = SPIFFS.open(filename);
+  if(file) {  // if file exists
+    deserializeJson(docRead, file); // Deserialize Data
+  }
+  file.close();
+
+  printFileData();
+}
+
+// Prints values stored in docRead from memory
+void printFileData(){
+  Serial.println("Acceleration X DATA!!!!!");
+  for(int i=0; i<docRead["acc"]["x"].size(); i++){
+    Serial.print((double)docRead["acc"]["x"][i]);
+    Serial.print(",");
+  }
+  Serial.println("\nAcceleration Y DATA!!!!");
+  for(int i=0; i<docRead["acc"]["y"].size(); i++){
+    Serial.print((double)docRead["acc"]["y"][i]);
+    Serial.print(",");
+  }
+  Serial.println("\nAcceleration Z DATA!!!!");
+  for(int i=0; i<docRead["acc"]["z"].size(); i++){
+    Serial.print((double)docRead["acc"]["z"][i]);
+    Serial.print(",");
+  }
+  Serial.println("");
+}
+
+// saves the acceleration and rotation data during the recording time period in the memory
+void writeDataToFile() {
+  File outfile = SPIFFS.open(filename,"w"); //set to write data
+  
+  // Serialize and write data to SPIFFS
+  if(serializeJson(docWrite, outfile)==0) {
+    Serial.println("Failed to write to SPIFFS file");
+  } else {
+    Serial.println("Write Success!");
+  }
+  outfile.close();  
 }
